93f10276dee196d73e11352f3e9153139d7e612e
Mark Mendoza <57791540+markmendozadev@users.noreply.github.com>
2022-04-05 16:39:17 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     domains: ["image.tmdb.org"],
+    disableStaticImages: true,
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPlugins([nextConfig, optimizedImages]);
+
+// module.exports = withPlugins([optimizedImages]);
